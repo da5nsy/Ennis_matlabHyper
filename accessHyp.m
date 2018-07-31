fe67b6d20636bbe2df11e504e81b00ba45793ff7
da5nsy <dannygarside@outlook.com>
2018-07-31 14:04:25 +0100

    problem at low wavelengths

--- accessHyp.m
+++ accessHyp.m
@@ -21,8 +21,8 @@ fls = dir([fldr,'*','.mat']); %files
 rgb   = 1; %calculate rgb data for each dataset
 plt_i = 0; %plot individual white data
 
-% - % If you only want wlns and w (the spectrus) then:
-load('C:\Users\cege-user\Dropbox\Documents\MATLAB\Downloaded functions\matlabHyper\results.mat')
+% % - % If you only want wlns and w (the spectrus) then:
+% load('C:\Users\cege-user\Dropbox\Documents\MATLAB\Downloaded functions\matlabHyper\results.mat')
 
 wlns = csvread('hyperWavelengths.csv');
 wlns = wlns(20:364);
@@ -72,32 +72,16 @@ end
 
 avo_white = squeeze(fls(3).hyper(52,181,:));
 
-%%
-for i=39 %39 is a red pepper and gives a weird white rating because the stem is included in the ~mask space
-    %grgb is complex for some reason, and so I'm brute forcing the non
-    %   gamma corrected image
-    figure
-%    imagesc(fls(i).rgb*50)
-    imagesc((fls(i).rgb(1:45,:,:).*~fls(i).mask(1:45,:,:))*20)
-end
-axis equal
 
-%% Check colorimetry of white vs the colorimetry quoted in the paper
-
-load T_xyz1931.mat %PsychToolbox
-XYZ = median(w,2)'*interp1(SToWls(S_xyz1931),T_xyz1931',wlns); %PTB
-xy = [XYZ(1)/sum(XYZ);XYZ(2)/sum(XYZ)];
-Y = XYZ(2) * 683;
-
-%that's roughly in line with what I would expect from taking roughly 80% of
-%the power out of the lights from refelctions
 
 %% Pick out areas of each image from where a reflectance could be calculated
 
-i=10;
-figure,
-imagesc(fls(i).grgb.*fls(i).mask) %show original image, zoom in and select
-axis equal
+close all
+
+% i=10;
+% figure,
+% imagesc(fls(i).grgb.*fls(i).mask) %show original image, zoom in and select
+% axis equal
 
 %avoiding specular highlights, aiming for normal of 45deg, which
 %practically means just aiming for the upper half of the image
@@ -117,61 +101,42 @@ fls(10).hs = fls(10).hyper(100:150, 360:460,:);
 % Would be good to visualise this to check that I've picked sensible areas
 %   and haven't typo'd
 
-% figure, hold on
-% plot(reshape(fls(2).hs,61*101,345)','k') %plot hs
-% fls(2).av= median(reshape(fls(2).hs,61*101,345));
-% plot(fls(2).av,'r');
-
-figure, hold on
-for i=1:length(fls)
-    fls(i).av= median(reshape(fls(i).hs,size(fls(i).hs,1)*size(fls(i).hs,2),345));
-    fls(i).av_i = interp1(wlns,fls(i).av,380:780);
+plt_i = 0;
+figure(100), hold on
+plot(380:780,light,'DisplayName','Spectralon (Ennis)') %Pre-plot light
+for i=1:10
     
+    if plt_i
+        figure(i),hold on
+        plot(reshape(fls(i).hs,size(fls(i).hs,1)*size(fls(i).hs,2),345)','k') %plot full hs
+        plot(fls(i).av,'r');
+        title(fls(i).name(1:regexp(fls(i).name,'_')-1));
+    end
+    
+    fls(i).av=median(reshape(fls(i).hs,size(fls(i).hs,1)*size(fls(i).hs,2),345));
+        
+    fls(i).av_i = interp1(wlns,fls(i).av,380:780);
     fls(i).av_i(isnan(fls(i).av_i)) = 0;
     
-    plot(380:780,fls(i).av_i)
+    figure(100),
+    plot(380:780,fls(i).av_i,'DisplayName',fls(i).name(1:regexp(fls(i).name,'_')-1))
 end
 title('Average spectral radiance from chosen patches')
-
-%% Compare white and light
-
-load('C:\Users\cege-user\Downloads\thorlabs.mat');
-light = spectra(1).spectralData; clear spectra
-
-load('C:\Users\cege-user\Dropbox\Documents\MATLAB\Downloaded functions\matlabHyper\results.mat');
-wht = w(:,1); clear w
-
-figure, hold on
-plot(380:780,light,'DisplayName','Light (Ennis)');
-plot(wlns,wht,'DisplayName','White wall, measured from images');
-plot(wlns,avo_white,'DisplayName','Avo white')
-
 legend('Location','Best')
 
 %% Calculate refelctance using light measurement from Ennis
-%       and then using white measurement taken from back wall of cube in hyp images
-clc
+%   which was measurement of spectralon, which should be pretty
+%   non-spectrally-selective
+%   (see https://github.com/terraref/reference-data/blob/master/Zenith%20LiteT%20Diffuse%20Reflectance%20Target%20-%2095%25R.pdf)
+
 colors = hsv(10);
 
 figure, hold on
-for i=10%1:10
+for i=1:10
     fls(i).ref = fls(i).av_i./light';    
-    plot(380:780,fls(i).ref,'Color',colors(i,:),'DisplayName','Light (Ennis)');
-
-    fls(i).ref = fls(i).av./wht';
-    plot(wlns,fls(i).ref,':','Color',colors(i,:),'DisplayName','Sampled white');
+    plot(380:780,fls(i).ref,'Color',colors(i,:),'DisplayName',fls(i).name(1:regexp(fls(i).name,'_')-1))
 end
+xlim([380 780])
+ylim([0 1])
 
 legend('Location','Best')
-
-% Comparing to apple spd in doi: 10.1016/S0925-5214(02)00066-2
-% This apple is red/yellow
-
-%%
-wht_i = interp1(wlns,wht,380:780);
-wht_i(isnan(wht_i))=0;
-
-figure,
-plot(wht_r)
-
-wht_r = wht_i./light';
